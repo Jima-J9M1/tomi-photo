0e2b422d8f6238ed511d07aa1e4c57602a48854b
jima <jimd3730@gmail.com>
2025-02-11 14:15:29 +0300

    bug: fix the mobile view video

--- src/components/ui/video.tsx
+++ src/components/ui/video.tsx
@@ -1,7 +1,7 @@
-export default function Video({src}:{src:string}): React.ReactNode {
-    return (
-    <video width="100%" height="100%" loop autoPlay muted>
-      <source src={src} type="video/mp4" />
+export default function Video({ src }: { src: string }) {
+  return (
+    <video className="w-full h-full" loop autoPlay muted>
+      <source src={src} type="video/mp4"/>
     </video>
-    );
-  }
+  );
+}
